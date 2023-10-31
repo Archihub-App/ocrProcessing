b05fd43ec80cd7bd892aa5a4bc1584d6c4575cdb
Nestor Pena <nestor2509@gmail.com>
2023-10-31 08:52:03 -0500

    avances en la gestion de carpetas

--- __init__.py
+++ __init__.py
@@ -4,7 +4,6 @@ from celery import shared_task
 from app.utils import DatabaseHandler
 from flask import request
 import os
-from pycocotools.coco import COCO
 import layoutparser as lp
 import cv2
 from app.api.records.models import RecordUpdate
@@ -73,8 +72,6 @@ class ExtendedPluginClass(PluginClass):
         if len(records) > 0:
             model = lp.Detectron2LayoutModel('/home/nestor/.torch/iopath_cache/s/57zjbwv6gh3srry/config.yaml',
                                              '/home/nestor/.torch/iopath_cache/s/57zjbwv6gh3srry/model_final.pth',
-                                             extra_config=[
-                                                 "MODEL.ROI_HEADS.SCORE_THRESH_TEST", 0.8],
                                              label_map={0: "Text", 1: "Title", 2: "List", 3: "Table", 4: "Figure"})
             ocr_agent = lp.TesseractAgent(languages='spa')
 
